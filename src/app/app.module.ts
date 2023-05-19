888074260723009e04ead4c691af496f385f67d8
mohitpainuly88 <mohit@avioxtechnologies.com>
2023-05-19 10:37:17 +0530

    changes for borrower details page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
@@ -11,7 +12,10 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { MatTableModule } from '@angular/material/table';
 import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
-import {MatCardModule} from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatDialogModule } from '@angular/material/dialog';
 
 import {
   MsalGuard,
@@ -24,15 +28,24 @@ import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { HttpClientModule } from '@angular/common/http';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { BorrowDetailsComponent } from './borrow-details/borrow-details.component';
+import {MatChipsModule} from '@angular/material/chips';
+import {MatSelectModule} from '@angular/material/select';
+import {MatGridListModule} from '@angular/material/grid-list';
+// import { MatDialog } from '@angular/material/dialog';
+import { ErrorDialogComponent } from './error-dialog/error-dialog.component';
+
+
 
 const isIE =
   window.navigator.userAgent.indexOf('MSIE ') > -1 ||
   window.navigator.userAgent.indexOf('Trident/') > -1;
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, DashboardComponent, BorrowDetailsComponent],
+  declarations: [AppComponent, HomeComponent, DashboardComponent, BorrowDetailsComponent, ErrorDialogComponent],
   imports: [
     BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
     BrowserAnimationsModule,
     AppRoutingModule,
     HttpClientModule,
@@ -40,8 +53,14 @@ const isIE =
     MatToolbarModule,
     MatListModule,
     MatTableModule,
+    MatIconModule,
     MatProgressSpinnerModule,
-    MatCardModule,
+    MatFormFieldModule,
+    MatChipsModule,
+    MatSelectModule,
+    MatInputModule,
+    MatGridListModule,
+    MatDialogModule,
     MsalModule.forRoot(
       new PublicClientApplication({
         auth: {
--- src/app/borrow-details/borrow-details.component.ts
+++ src/app/borrow-details/borrow-details.component.ts
@@ -1,15 +1,85 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import {MatDialog} from '@angular/material/dialog';
+import { ErrorDialogComponent } from '../error-dialog/error-dialog.component';
 
 @Component({
   selector: 'app-borrow-details',
   templateUrl: './borrow-details.component.html',
-  styleUrls: ['./borrow-details.component.css']
+  styleUrls: ['./borrow-details.component.css'],
 })
 export class BorrowDetailsComponent implements OnInit {
+  @ViewChild('mapElement', { static: true }) mapElementRef: ElementRef | undefined;
+  typeOfError:boolean=false
+  constructor(public dialog: MatDialog) {}
 
-  constructor() { }
+  aadharCardImg: string = '../../assets/image/aadharCard.jpg';
+  panCardImg: string = "../../assets/image/pan-card.jpg";
+  passport: string = "../../assets/image/passport.jpg";
+  verified: string = "../../assets/image/verified.png";
+  unverified: string = "../../assets/image/unverified.png";
+  totaldoc: string = "../../assets/image/total-doc.png";
+  images: string[] = [this.aadharCardImg, this.panCardImg, this.passport];
 
-  ngOnInit(): void {
+
+  ngOnInit(): void {}
+  currentIndex: number = 0;
+
+  toppingsControl = new FormControl([]);
+  toppingList: string[] = ['Extra cheese', 'Mushroom', 'Onion', 'Pepperoni', 'Sausage', 'Tomato'];
+
+  options = ['Option 1', 'Option 2', 'Option 3', 'Option 4'];
+  selectedOptions: any;
+
+  removeOption(option: string) {
+    const index = this.selectedOptions.indexOf(option);
+    if (index >= 0) {
+      this.selectedOptions.splice(index, 1);
+    }
+  }
+
+  prevImage(): void {
+    this.currentIndex =
+      this.currentIndex === 0 ? this.images.length - 1 : this.currentIndex - 1;
+  }
+
+  nextImage(): void {
+    this.currentIndex =
+      this.currentIndex === this.images.length - 1 ? 0 : this.currentIndex + 1;
+  }
+
+  ReportError():void{
+    this.typeOfError =true
+
+  }
+  confirm():void {
+     this.typeOfError =false
+
+  }
+  openDialog() {
+     this.dialog.open(ErrorDialogComponent, {
+      maxWidth: '60vw',
+      maxHeight: '60vh',
+      height: '60%',
+      width: '60%',
+      data: {
+        approvedDoc: '12',
+        totalDoc: '25',
+      },
+
+    });
+  }
+
+  zoom = 1;
+
+  zoomIn() {
+    this.zoom += 0.1;
+  }
+
+  zoomOut() {
+    if (this.zoom > 0.1) {
+      this.zoom -= 0.1;
+    }
   }
 
 }
