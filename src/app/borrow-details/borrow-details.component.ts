5b12eb5b75e69f8064b68127b5286ff6da095e96
mohitpainuly88 <mohit@avioxtechnologies.com>
2023-05-22 09:48:22 +0530

    changes for borrow details

--- src/app/borrow-details/borrow-details.component.ts
+++ src/app/borrow-details/borrow-details.component.ts
@@ -19,16 +19,16 @@ export class BorrowDetailsComponent implements OnInit {
   verified: string = "../../assets/image/verified.png";
   unverified: string = "../../assets/image/unverified.png";
   totaldoc: string = "../../assets/image/total-doc.png";
-  images: string[] = [this.aadharCardImg, this.panCardImg, this.passport];
+  zoomInImage: string = "../../assets/image/zoom-in.png";
+  zoomOutImage: string = "../../assets/image/zoom-out.png";
 
+  images: string[] = [this.aadharCardImg, this.panCardImg, this.passport];
+  categoriesControl = new FormControl([]);
+  categories: string[] = ['Option 1', 'Option 2', 'Option 3', 'Option 4'];
+  options = ['Option 1', 'Option 2', 'Option 3', 'Option 4'];
 
   ngOnInit(): void {}
   currentIndex: number = 0;
-
-  toppingsControl = new FormControl([]);
-  toppingList: string[] = ['Extra cheese', 'Mushroom', 'Onion', 'Pepperoni', 'Sausage', 'Tomato'];
-
-  options = ['Option 1', 'Option 2', 'Option 3', 'Option 4'];
   selectedOptions: any;
 
   removeOption(option: string) {
@@ -79,6 +79,19 @@ export class BorrowDetailsComponent implements OnInit {
   zoomOut() {
     if (this.zoom > 0.1) {
       this.zoom -= 0.1;
+    }
+  }
+  
+  onCatRemoved(cat: string) {
+    const categories = this.categoriesControl.value as never[];
+    this.removeFirst(categories, cat);
+    this.categoriesControl.setValue(categories); // To trigger change detection
+  }
+
+  private removeFirst(array: any[], toRemove: any): void {
+    const index = array.indexOf(toRemove);
+    if (index !== -1) {
+      array.splice(index, 1);
     }
   }
 
